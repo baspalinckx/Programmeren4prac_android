6a65cde7a56dff3170c13c32498c7f092f05451e
SBS 6 <bas.palinckx@hotmail.nl>
2017-06-15 11:22:01 +0200

    apilink veranderd

--- app/src/main/java/nl/avans/android/todos/service/Config.java
+++ app/src/main/java/nl/avans/android/todos/service/Config.java
@@ -6,7 +6,7 @@ package nl.avans.android.todos.service;
 
 public class Config {
 
-    public static final String URL_LOGIN = "https://mynodetodolistserver.herokuapp.com/api/v1/login";
+    public static final String URL_LOGIN = "https://progprac.herokuapp.com/api/v1/login";
     public static final String URL_TODOS = "https://mynodetodolistserver.herokuapp.com/api/v1/todos";
 
 }
