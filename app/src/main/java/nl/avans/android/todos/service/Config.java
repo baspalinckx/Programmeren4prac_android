2a156684c626db19bcfcbd3ef76da546e43695bd
SBS 6 <bas.palinckx@hotmail.nl>
2017-06-15 14:09:33 +0200

    Registreren werkt, layout moet nog netjes

--- app/src/main/java/nl/avans/android/todos/service/Config.java
+++ app/src/main/java/nl/avans/android/todos/service/Config.java
@@ -7,7 +7,7 @@ package nl.avans.android.todos.service;
 public class Config {
 
     public static final String URL_LOGIN = "https://progprac.herokuapp.com/api/v1/login";
-    public static final String URL_REGISTER = "http://localhost:3000/api/v1/register";
+    public static final String URL_REGISTER = "https://progprac.herokuapp.com/api/v1/register";
     public static final String URL_TODOS = "https://mynodetodolistserver.herokuapp.com/api/v1/todos";
 
 }
