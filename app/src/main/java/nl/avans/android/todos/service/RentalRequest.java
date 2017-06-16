0dc215050e5063611d5f0c8002db6c06f50c9efa
SBS 6 <bas.palinckx@hotmail.nl>
2017-06-16 14:34:15 +0200

    Wilde beginnen aan een nieuwe rental request, maar begreep niet hou we aan inventoryid moet komen

--- app/src/main/java/nl/avans/android/todos/service/RentalRequest.java
+++ app/src/main/java/nl/avans/android/todos/service/RentalRequest.java
@@ -87,6 +87,49 @@ public class RentalRequest {
         }
     }
 
+    //deze methode moet een rental gaan toevoegen, maar snap niet helemaal waar we inventory id vandaan halen.
+    public void handleAddRental(int customerId) {
+
+        Log.i(TAG, "handleGetAllToDos");
+
+        // Haal het token uit de prefs
+        SharedPreferences sharedPref = context.getSharedPreferences(
+                context.getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        final String token = sharedPref.getString(context.getString(R.string.saved_token), "dummy default token");
+        if(token != null && !token.equals("dummy default token")) {
+
+            Log.i(TAG, "Token gevonden, we gaan het request uitvoeren");
+            JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                    (Request.Method.POST, "https://progprac.herokuapp.com/api/v1/rentals/" + customerId,null, new Response.Listener<JSONObject>() {
+
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            // Succesvol response
+                            Log.i(TAG, response.toString());
+                            ArrayList<Rental> result = RentalMapper.mapRentalList(response);
+                            listener.onRentalsAvailable(result);
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            //handleErrorResponse(error);
+                            Log.e(TAG, error.toString());
+                        }
+                    }){
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> headers = new HashMap<>();
+                    headers.put("Content-Type", "application/json");
+                    headers.put("Authorization", "Bearer " + token);
+                    return headers;
+                }
+            };
+
+            // Access the RequestQueue through your singleton class.
+            VolleyRequestQueue.getInstance(context).addToRequestQueue(jsObjRequest);
+        }
+    }
+
 //    public void handlePostToDo(final Film newFilm) {
 //
 //        Log.i(TAG, "handlePostToDo");
