8b2dae41b69ab9053f943b935850711d19250609
koen99 <kjwdamme@avans.nl>
2017-06-17 13:01:01 +0200

    actionbar gefixt

--- app/src/main/java/nl/avans/android/todos/presentation/MainActivity.java
+++ app/src/main/java/nl/avans/android/todos/presentation/MainActivity.java
@@ -52,8 +52,8 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -65,11 +65,11 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//        drawer.setDrawerListener(toggle);
-//        toggle.syncState();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -224,7 +224,7 @@ public class MainActivity extends AppCompatActivity
     public void getRentals() {
         Intent intent = getIntent();
         int customerId = (Integer) intent.getSerializableExtra("ID");
-        
+
         RentalRequest request = new RentalRequest(getApplicationContext(), this);
         request.handleGetAllRentals(customerId);
     }
